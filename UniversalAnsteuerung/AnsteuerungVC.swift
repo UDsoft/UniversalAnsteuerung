b3a9019e09eb9681b5cd9c45713d9734c8556220
darwin subramaniam <darwin.subramaniam@stud.hn.de>
2016-12-21 23:02:00 +0100

    antsteuerung design and basic programming added function of switch for touch permission

--- UniversalAnsteuerung/AnsteuerungVC.swift
+++ UniversalAnsteuerung/AnsteuerungVC.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class AnsteuerungVC: UIViewController {
-
+class AnsteuerungVC: UIViewController , UIPickerViewDataSource,UIPickerViewDelegate{
+    
+    @IBOutlet weak var xAchsePicker: UIPickerView!
+    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var yAchsePicker: UIPickerView!
+    @IBOutlet weak var zAchsePicker: UIPickerView!
+    @IBOutlet weak var a1SwitchOutlet: UISwitch!
+    @IBOutlet weak var a2SwitchOutlet: UISwitch!
+    @IBOutlet weak var a3SwitchOutlet: UISwitch!
+    
+    
+    
+    var pickerData = ["0"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dataBasePopulator(firstValue: 1, lastValue: 100)
+        xAchsePicker.dataSource = self
+        xAchsePicker.delegate = self
+        yAchsePicker.dataSource = self
+        yAchsePicker.delegate = self
+        zAchsePicker.dataSource = self
+        zAchsePicker.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +39,63 @@ class AnsteuerungVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if(pickerView.tag == 1){
+            testLabel.text = "1 " + pickerData[row]
+        }else if(pickerView.tag == 2){
+            testLabel.text = "2 " + pickerData[row]
+        }else if(pickerView.tag == 3){
+            testLabel.text = "3 " + pickerData[row]
+        }
+    }
 
+    
+    @IBAction func a1SwitchAction(_ sender: UISwitch) {
+        if(a1SwitchOutlet.isOn){
+            xAchsePicker.isUserInteractionEnabled = true
+        }else{
+            xAchsePicker.isUserInteractionEnabled = false
+        }
+    }
+    
+    @IBAction func a2SwitchAction(_ sender: UISwitch) {
+        if(a2SwitchOutlet.isOn){
+            yAchsePicker.isUserInteractionEnabled = true
+        }else{
+            yAchsePicker.isUserInteractionEnabled = false
+        }
+    }
+
+    @IBAction func a3SwitchAction(_ sender: UISwitch) {
+        if(a3SwitchOutlet.isOn){
+            zAchsePicker.isUserInteractionEnabled = true
+        }else{
+            zAchsePicker.isUserInteractionEnabled = false
+        }
+        
+    }
+    
+    func dataBasePopulator(firstValue:Int,lastValue:Int){
+        for var x in firstValue...lastValue{
+            pickerData.append(String(x))
+        }
+
+    }
+    
     /*
     // MARK: - Navigation
 
